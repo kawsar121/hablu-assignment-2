0bf72b8bc637ceb832f0aa508dfa51f8d9ca0222
kawsar121 <tohidulislamkawsarbhuiyan2567gmail.com>
2024-05-31 22:42:52 +0600

    all are done

---
+++ name.js
@@ -0,0 +1,80 @@
+   // Problem:1
+let Carname = "Volvo";
+
+    // Problem:2
+let firstName = "John";
+let lastName = "Doe";
+let age = 35 ;
+
+    // *Problem:3
+x = 10;
+y = 5;
+x = x*y;
+// console.log(x) Ans:50
+
+    // *Problem:4
+let length = 16; // data type : integer
+let lastName = "Johnson"; // data type : string 
+    
+    const x = {
+      firstName: "John",  
+      lastName: "Doe"
+    };    // data type : object
+
+
+    // *problem:5
+
+function myFunction() {
+    alert("Hello World!");
+  }
+  myFunction();
+
+    //  *Problem:6
+
+const person = {
+    name : "John",
+    age  : 50,
+}
+
+alert(person.name + ' is ' + person.age)
+
+
+    // *Problem 7
+
+function myFunction() {
+    alert('thanks for click me')
+}
+
+    // *problem 8
+
+// *1
+const cars = ["Volvo", "Jeep", "Mercedes"];
+alert('Array property ' +cars.length)
+
+// *2
+const Brand = ["Ford", "Jeep", "Mercedes"]; 
+
+    // Problem 9
+//*1
+let number = Math.random()
+console.log(number)
+
+//*2
+let mathMax = Math.max(20,30)
+console.log('the largest number is: ' + mathMax)
+
+//*3
+let mathSqre = Math.sqrt(9)
+console.log('square root: ' + mathSqre)
+
+    // Problem 10
+//*1
+    x = 10;
+    y = 5;
+    z = x>y;
+    alert(z)
+
+//*2
+let age = 16;
+let compare = age < 18 ? "Too young" :  "Old enough"
+alert(compare)
